60889db2838f4eb973a40fa7b708e989fe4ba24e
Jay <jpnappy@gmail.com>
2016-10-23 14:20:15 -0400

    first commit

---
+++ cool.rb
@@ -0,0 +1,9 @@
+# write a function that accepts one parameter, location
+#if the location is equal to brooklyn, show the user a message "you are so cool"
+#if the location is equal to manhattan, show the user a message "you are cool"
+#in all other circumstances, show a message that says "you are not cool" 
+
+# ask the user for where they live
+# store that information in a variable
+
+# call the fuction
