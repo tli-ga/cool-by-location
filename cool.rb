517001a50aef4545f8c87ddaa876347d48d83682
Tiffany Li <tiffanyli@Tiffanys-MacBook-Air.local>
2016-10-23 14:48:34 -0400

    look at this thing

--- cool.rb
+++ cool.rb
@@ -1,9 +1,23 @@
 # write a function that accepts one parameter, location
+
+def cool_location(location)
+	if location == "brooklyn" or location == "Brooklyn"
+		puts("You are so cool")
+	elsif location == "manhattan" or location == "Manhattan"
+		puts("You are cool")
+	else 
+		puts("You are not cool")
+	end
+end
 #if the location is equal to brooklyn, show the user a message "you are so cool"
 #if the location is equal to manhattan, show the user a message "you are cool"
 #in all other circumstances, show a message that says "you are not cool" 
 
+puts("Where do you live?")
 # ask the user for where they live
+
+user_location = gets.strip
 # store that information in a variable
 
+cool_location(user_location)
 # call the fuction
